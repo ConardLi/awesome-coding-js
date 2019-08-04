2b36c5d4705040b6016436210d3545bc5b679a1d
lishiqi <1009903985@qq.com>
2019-08-04 17:35:22 +0800

    art update: heap

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,25 +1,18 @@
 module.exports = {
     title: 'awesome-coding-js',
     description: '用JavaScript实现的算法和数据结构',
+    base: '/docs/',
     themeConfig: {
         sidebarDepth: 2,
         lastUpdated: 'Last Updated',
         nav: [
             { text: '数据结构分类', link: '/dataStructure/' },
-            { text: '博客', link: 'http://www.conardli.top' },
+            { text: '博客', link: 'http://www.conardli.top/blog/article/' },
             { text: 'github', link: 'https://github.com/ConardLi' },
         ],
         sidebar: {
             '/dataStructure/': [
                 '/dataStructure/',
-                {
-                    title: '堆',
-                    children: [
-                        '/dataStructure/堆/堆的基本操作',
-                        '/dataStructure/堆/数据流中的中位数',
-                        '/dataStructure/堆/最小的k个数',
-                    ]
-                },
                 {
                     title: '二叉树',
                     children: [
@@ -41,7 +34,29 @@ module.exports = {
                         '/dataStructure/二叉树/二叉树的下一个节点',
                         '/dataStructure/二叉树/树的子结构',
                     ]
-                }
+                },
+                {
+                    title: '堆',
+                    children: [
+                        '/dataStructure/堆/堆的基本操作',
+                        '/dataStructure/堆/数据流中的中位数',
+                        '/dataStructure/堆/最小的k个数',
+                    ]
+                },
+                {
+                    title: '链表',
+                    children: [
+                        '/dataStructure/链表/从尾到头打印链表',
+                        '/dataStructure/链表/反转链表',
+                        '/dataStructure/链表/复杂链表的复制',
+                        '/dataStructure/链表/合并两个排序的链表',
+                        '/dataStructure/链表/链表倒数第k个节点',
+                        '/dataStructure/链表/链表中环的入口节点',
+                        '/dataStructure/链表/两个链表的第一个公共节点',
+                        '/dataStructure/链表/圈圈中最后剩下的数字',
+                        '/dataStructure/链表/删除链表中的节点or重复的节点',
+                    ]
+                },
             ]
         }
     }
